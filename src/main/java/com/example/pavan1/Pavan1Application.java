e1e1693c65d4e3f11310bfcb249dcf1f83a9ad00
pavankatari143 <pavan.k@suntechnoservices.com>
2020-03-05 15:21:27 -0800

    pavan

---
+++ src/main/java/com/example/pavan1/Pavan1Application.java
@@ -0,0 +1,14 @@
+package com.example.pavan1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pavan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pavan1Application.class, args);
+		System.out.print("pavan is a good person");
+	}
+
+}
---
+++ src/test/java/com/example/pavan1/Pavan1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pavan1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pavan1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
